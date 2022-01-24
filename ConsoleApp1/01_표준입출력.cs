4f228c6829a61d579b1327a3b6a6311b70711f7e
tebin <ohjiwoo123@naver.com>
2022-01-24 16:27:25 +0900

    초기 커밋.

---
+++ ConsoleApp1/01_표준입출력.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 출력----------------------------------------------------------
+            // WriteLine
+            if (false)
+            {
+                Console.WriteLine("Hello C#\n");
+
+                int i = 1234;
+                double d = 5.6789;
+                string s = "문자열";
+
+                Console.WriteLine(i);
+                Console.WriteLine(d);
+                Console.WriteLine(s);
+                Console.WriteLine("------------------------");
+            }
+            // Write
+            // WriteLine
+            if (false)
+            {
+                Console.WriteLine("헬로우 C#\n");
+
+                int i = 1234;
+                double d = 5.6789;
+                string s = "문자열";
+
+                Console.Write(i);
+                Console.Write(d);
+                Console.Write(s);
+                Console.WriteLine("------------------------");
+            }
+            // stringFormat
+            if (false)
+            {
+                int i = 1234;
+                double d = 5.6789;
+
+                Console.WriteLine("정수는 " + i + "이고 실수는 " + d + "이다");
+                Console.WriteLine("정수는 {0}이고 실수는 {1}이다.", i, d);
+                // X : 핵사로 찍어달라, F2 : 소수점 2자리까지 
+                Console.WriteLine("정수는 {0:X}이고 실수는 {1:F2}이다.", i, d);
+                Console.WriteLine("------------------------");
+            }
+            // Interpolation (문자열 보간) $
+            if (false)
+            {
+                int age = 18;
+                string name = "김한솔";
+
+                Console.WriteLine("안녕하세요 {0}세 {1}입니다.", age, name);
+                Console.WriteLine($"안녕하세요 {age + 2}세 {name}입니다.");
+                Console.WriteLine("------------------------");
+            }
+
+            // 입력--------------------------------------------------------------
+            // ReadLine
+            if (false)
+            {
+                Console.Write("이름을 입력하세요 : ");
+                string Name = Console.ReadLine();
+
+                Console.Write("나이을 입력하세요 : ");
+                string sAge = Console.ReadLine();
+                int Age = Convert.ToInt32(sAge);
+
+                Console.WriteLine($"안녕하세요 {Age}세 {Name}님");
+                Console.WriteLine("---------------------------");
+            }
+            // ReadKey
+            if (false)
+            {
+                ConsoleKeyInfo cki;
+                int x = 40, y = 12;
+
+                for(; ; )
+                {
+                    Console.Clear();
+                    Console.SetCursorPosition(x, y);
+                    Console.Write('#');
+
+                    cki = Console.ReadKey(true);
+                    switch(cki.Key)
+                    {
+                        case ConsoleKey.LeftArrow:  x--;     break;
+                        case ConsoleKey.RightArrow: x++;     break;
+                        case ConsoleKey.UpArrow:    y--;     break;
+                        case ConsoleKey.DownArrow:  y++;     break;
+                        case ConsoleKey.Q: return;
+                    }
+                }
+            }
+            // Console Member
+            // Console = cout + cin + 콘솔하면전체 제어 
+            if (false)
+            {
+                Console.Title = "콘솔 테스트";
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Clear();
+                Console.Beep();
+                Console.WriteLine("반가워요");
+                Console.ReadLine(); // enter 입력
+                Console.ResetColor();
+                Console.SetCursorPosition(10, 10);
+                Console.WriteLine("디폴트 색상입니다.");
+
+                Console.WriteLine("---------------------");
+            }
+
+            // 네임스페이스 ------------------------------------
+;            if (true)
+            {
+                System.Console.WriteLine("안녕하세요.");
+                Console.WriteLine("안녕하세요.");
+                WriteLine("안녕하세요.");
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/02_데이터타입.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// 
+/// 데이터 타입
+/// 1. 기본 데이터 타입(내장형)
+//// 1) 정수형 - int
+//// 2) 실수형 - float, double
+//// 3) 논리형 - BOOL(boolean)
+//// 4) 문자형 - char
+/// 2. 사용자 정의 데이터 타입
+//// 1) 열거형(enum)
+//// 2) 구조체(struct)
+//// 3) 클래스(class)
+/// 3. 배열(Array : [])
+/// vs 링크드리스트(LinkedList)
+/// vs 스택과 큐(Stack & Queue)
+
+namespace ConsoleApp1
+{
+    enum DAY { SUN,MON,TUE=9,THUR,FRI,SAT }
+    enum Direction { East=10, West, South, North }
+
+    // 구조체 : 이형의 자료를 모아서 관리함
+    struct Book
+    {
+        public string Name;
+        public int Price;
+    };
+
+    // 구조체의 발전 : 자료 + 함수 (-->OOP의 1단계 : 캡슐화)
+    // 2,3단계는 따라갈 수 없음
+    struct Book2
+    {
+        public string Name;
+        public int Price;
+
+        // 생성자는 리턴이 없다.
+        public Book2(string a, int b)
+        {
+            Name = a;
+            Price = b;
+        }
+        public Book2(string a, double b)
+        {
+            Name = a;
+            Price = (int)b;
+        }
+        public void PrintBook()
+        {
+            Console.WriteLine($"이름 = {Name}, 가격 = {Price}");
+        }
+    }
+    internal class _02_데이터타입
+    {
+        static void Main(string[] args)
+        {
+            // 열거형
+            if (false)
+            {
+                //int myBirthDay = (int)DAY.SAT;
+                //Console.Write(myBirthDay);
+
+                DAY myBirthDay;
+                myBirthDay = DAY.SAT;
+
+                Direction myHouse = Direction.North;
+                string temp = myHouse.ToString();
+
+                Console.WriteLine(myBirthDay);
+                Console.WriteLine((int)myHouse);
+                Console.WriteLine(myHouse);
+                Console.WriteLine(temp);
+
+            }
+            // 구조체
+            if (false)
+            {
+                Book b;
+                b.Name = "떼돈벌기";
+                b.Price = 18000;
+                Console.WriteLine($"제목 {b.Name}\n가격:{b.Price}");
+                Console.WriteLine("------------------------------");
+
+                // 생성자를 부르고 있다.
+                Book2 bb = new Book2("삐삐",2500);
+                bb.PrintBook();
+                Console.WriteLine("------------------------------");
+            }
+            // 배열
+            if (false)
+            {
+                // C# 은 포인터가 없다.
+                int[] ar = new int[5];
+
+                for(int i =0; i<5;i++)
+                {
+                    Console.WriteLine(ar[i]);
+                }
+                Console.WriteLine("------------------------------");
+                for (int i = 0; i < ar.Length; i++)
+                {
+                    ar[i] = i*2;
+                    Console.WriteLine(ar[i]);
+                }
+                // Console.WriteLine(ar); System.Int32[] 가 출력됨.
+            }
+            // 배열2
+            if (true)
+            {
+                int[] arScore = { 89,88,23,56,22,90,100 };
+                Console.Write("정렬전 ==> ");
+                for (int i=0; i<arScore.Length; i++)
+                {
+                    Console.Write(arScore[i]+", ");
+                }
+                Console.WriteLine("\n------------------------------");
+                Console.Write("정렬후 ==> ");
+                System.Array.Sort(arScore);
+                for (int i = 0; i < arScore.Length; i++)
+                {
+                    Console.Write(arScore[i] + ", ");
+                }
+            }
+        }
+    }
+}
